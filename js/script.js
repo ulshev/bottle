d4531034bb19bae42fa60b9ce5a38654d3c2bb3d
Ulyana Shevchenko <shev.ulya@gmail.com>
2017-03-10 10:15:03 +0400

    Внутренние: все кроме продукция-главная, 404

--- js/script.js
+++ js/script.js
@@ -1,45 +1,17 @@
 function tweakSlider() {
         var width = $(window).width();
-	if (width > 975) {
-	    $('.main_menu').css("display", "block");
-	    
-	    $(".karusel_1").sudoSlider({
-		touch: true,
-		mouseTouch: true,
-		prevNext:false,
-		customLink:'.customLink1',
-		slideCount: 3,
-		continuous: true,
-		responsive: false,
-	    });
-	    
-	    if (width > 1250) {
-	    
-		$(".karusel_2").sudoSlider({
-		    touch: true,
-		    mouseTouch: true,
-		    prevNext:false,
-		    customLink:'.customLink2',
-		    slideCount: 6,
-		    continuous: true,
-		       responsive: false,
-	       });
-	    }else{
-		
-		$(".karusel_2").sudoSlider({
-		    touch: true,
-		    mouseTouch: true,
-		    prevNext:false,
-		    customLink:'.customLink2',
-		    slideCount: 4,
-		    continuous: true,
-		       responsive: false,
-	       });
-	    }
-	    
-	}else{
+	if (width < 975) {
 	    $('.main_menu').css("display", "none");
 	    
+	 $('.a_about').click(function(){
+		$(arguments).get(0).preventDefault();
+	    });
+	 $('.a_production').click(function(){
+		$(arguments).get(0).preventDefault();
+	    });
+	 $('.a_product').click(function(){
+		$(arguments).get(0).preventDefault();
+	    });
 	    
 	    if (width > 800) {
 		$(".karusel_1").sudoSlider({
@@ -82,41 +54,88 @@ function tweakSlider() {
 		       responsive: false,
 	       });
 	    }
+	    
+	}else{
+	    $('.main_menu').attr('style', '');
+	    $('.a_about').attr('style', '');
+	    $('.a_production').attr('style', '');
+	    $('.a_product').attr('style', '');
+	    
+	    $(".karusel_1").sudoSlider({
+		touch: true,
+		mouseTouch: true,
+		prevNext:false,
+		customLink:'.customLink1',
+		slideCount: 3,
+		continuous: true,
+		responsive: false,
+	    });
+	    
+	    if (width > 1250) {
+	    
+		$(".karusel_2").sudoSlider({
+		    touch: true,
+		    mouseTouch: true,
+		    prevNext:false,
+		    customLink:'.customLink2',
+		    slideCount: 6,
+		    continuous: true,
+		       responsive: false,
+	       });
+	    }else{
+		
+		$(".karusel_2").sudoSlider({
+		    touch: true,
+		    mouseTouch: true,
+		    prevNext:false,
+		    customLink:'.customLink2',
+		    slideCount: 4,
+		    continuous: true,
+		       responsive: false,
+	       });
+	    }
+	    
+	    
+	    
 	}
+	
+	
 };
 
 
 
 $(document).ready(function() {
      
-     $('.menu_batton_mob').click(function(){
-	    if ($('.main_menu').css("display") == "block") {
-	       $('.main_menu').slideUp(500);
-	   }else{
-	      $('.main_menu').slideDown(500);
-	   }
-	});
-     $('.a_about').click(function(){
-	    if ($('.menu_about').css("display") == "block") {
-	       $('.menu_about').slideUp(500);
-	   }else{
-	      $('.menu_about').slideDown(500);
-	   }
-	});
-     $('.a_production').click(function(){
-	    if ($('.menu_production').css("display") == "block") {
-	       $('.menu_production').slideUp(500);
-	   }else{
-	      $('.menu_production').slideDown(500);
-	   }
-	});
-     $('.a_product').click(function(){
-	    if ($('.menu_product').css("display") == "block") {
-	       $('.menu_product').slideUp(500);
-	   }else{
-	      $('.menu_product').slideDown(500);
-	   }
-	});
+	
+	$('.menu_batton_mob').click(function(){
+		if ($('.main_menu').css("display") == "block") {
+		   $('.main_menu').slideUp(500);
+	       }else{
+		  $('.main_menu').slideDown(500);
+	       }
+	    });
+	 $('.a_about').click(function(){
+		if ($('.menu_about').css("display") == "block") {
+		   $('.menu_about').slideUp(500);
+	       }else{
+		  $('.menu_about').slideDown(500);
+	       }
+	    });
+	 $('.a_production').click(function(){
+		if ($('.menu_production').css("display") == "block") {
+		   $('.menu_production').slideUp(500);
+	       }else{
+		  $('.menu_production').slideDown(500);
+	       }
+	    });
+	 $('.a_product').click(function(){
+		if ($('.menu_product').css("display") == "block") {
+		   $('.menu_product').slideUp(500);
+	       }else{
+		  $('.menu_product').slideDown(500);
+	       }
+	    });
+	 
      $(window).resize(function(){
 	tweakSlider();
      });
@@ -133,6 +152,17 @@ $(document).ready(function() {
          pause: 3000,
          auto:true,
     });
+    
+    
+    $(".photo_slider ul").sudoSlider({
+	touch: true,
+	mouseTouch: true,
+	prevNext:false,
+	customLink:'.photo_slider_link',
+	slideCount: 4,
+	continuous: true,
+	responsive: false,
+     });
      
     tweakSlider();
 });
